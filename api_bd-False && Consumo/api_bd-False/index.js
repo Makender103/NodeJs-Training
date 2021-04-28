395d7039b22c7fe97bf4817e2bf3848768adad68
Marc kender R. Jean Charles <makender103@gmail.com>
2021-04-27 22:50:10 -0300

    JWT Authemtication with token

--- api_bd-False && Consumo/api_bd-False/index.js
+++ api_bd-False && Consumo/api_bd-False/index.js
@@ -2,9 +2,37 @@ const express = require("express")
 const app = express()
 
 const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const jwt = require("jsonwebtoken")
+const JWTSecret = "lfhglkgkegkerkggkregkrgkrengkegkng"
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+function auth(req, res, next) {
+    const autToken = req.headers['authorization'];
+    
+    if(autToken != undefined) {
+        const bearer = autToken.split(' ');
+        
+        let token = bearer[1]
+        jwt.verify(token,JWTSecret, (err, data)=> {
+            if(err){
+                res.status(401);
+                res.json({err: "token invalida"})
+            } else {
+                req.token = token;
+                req.loggedUser = {id: data.id, email: data.id}
+                next()
+            }
+        });
+    }else {
+        res.status(401);
+        res.json({err: "without Token"})
+    }
+}
 
 
 let DB = {
@@ -31,12 +59,27 @@ let DB = {
             year: 2012,
             price: 604
         },
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Marc kender",
+            email: "makender103@gmail.com",
+            password: "node12345"
+        },
+        {
+            id: 2,
+            name: "laouni",
+            email: "mjcode@gmail.com",
+            password: "java123"
+        }
     ]
 }
 
-app.get("/games", (req, res)=> {
+app.get("/games", auth, (req, res)=> {
+    req.loggedUser
     res.statusCode = 200
-    res.json(DB.games)
+    res.json({user: req.loggedUser, games: DB.games})
 })
 
 app.get("/games/:id", (req, res)=> {
@@ -111,6 +154,41 @@ app.put("/games/:id", (req, res)=> {
         } else {
             res.sendStatus(400)
         }
+    }
+});
+
+app.post("/auth", (req, res)=> {
+    let { email, password }= req.body;
+
+    if (email != undefined) {
+
+        let user = DB.users.find(u =>u.email == email);
+        if (user != undefined) {
+
+            if (user.password == password) {
+
+                jwt.sign({id: user.id, email: user.email}, JWTSecret, {expiresIn: '48h'}, (err, token) =>{
+                    if (err) {
+                        res.status(400);
+                        res.json({err: "Falha interna"})
+                    } else{
+                        res.status(200);
+                        res.json({token: token})
+                    }
+                })
+            } else {
+                res.status(401);
+                res.json({err: "credencias invalidas"})
+            }
+
+        }else {
+            res.status(404)
+            res.json({err: "email não existe na base de dados"})
+        }
+
+    } else{
+        res.status(400);
+        res.send({err: " o email enviado é Invalido"})
     }
 })
 
